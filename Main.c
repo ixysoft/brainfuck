28a242322e002bf7909f799385b14a309a60c68c
ixysoft <1340058590@qq.com>
2017-02-17 18:43:23 +0800

    Add files via upload

--- Main.c
+++ Main.c
@@ -35,7 +35,7 @@
  *	命令模式:
  *		形式:
  *			brainfuck 选项 [文件]
- *		
+ *
  *		选项:
  *			-f 表示执行文件
  *			-c 表示执行命令
@@ -46,10 +46,10 @@
 #include<string.h>
 #include<stdlib.h>
 /*Linux下通过termios.h实现的读取字符*/
-#ifdef _WIN32 //Linux platform  
-	#include <conio.h>  
-#else  
-	#include <termios.h>  
+#ifdef _WIN32 //Linux platform
+	#include <conio.h>
+#else
+	#include <termios.h>
 #endif
 
 #define MODE_INIT 0x00000000
@@ -78,36 +78,36 @@ int buf_pos=0;//缓存指针的值
 char comment=0x00;//默认为没有注释
 
 Command cmd_link;//全局的命令列表
-  
-char _getch(void)  
-{  
-	#ifdef _WIN32  
+
+char Getch(void)
+{
+	#ifdef _WIN32
 	//无动作
-	#else   // 保存并修改终端参数  
-		struct termios stored_settings;  
-		struct termios new_settings;  
-		tcgetattr (0, &stored_settings);  
-		new_settings = stored_settings;  
-		new_settings.c_lflag &= (~ICANON);  
-		new_settings.c_cc[VTIME] = 0;  
-		new_settings.c_cc[VMIN] = 1;  
-		tcsetattr (0, TCSANOW, &new_settings);  
-	#endif  
-	int ret = 0;  
-	char c;  
-	#ifdef _WIN32  
-		c = getch();  
-	#else  
-		c = getchar();  
-		putchar('\b'); // 删除回显  
-	#endif  
-	printf("%c", c);  
-	#ifdef _WIN32  
+	#else   // 保存并修改终端参数
+		struct termios stored_settings;
+		struct termios new_settings;
+		tcgetattr (0, &stored_settings);
+		new_settings = stored_settings;
+		new_settings.c_lflag &= (~ICANON);
+		new_settings.c_cc[VTIME] = 0;
+		new_settings.c_cc[VMIN] = 1;
+		tcsetattr (0, TCSANOW, &new_settings);
+	#endif
+	int ret = 0;
+	char c;
+	#ifdef _WIN32
+		c = getch();
+	#else
+		c = getchar();
+		putchar('\b'); // 删除回显
+	#endif
+	printf("%c", c);
+	#ifdef _WIN32
 	//没有动作
 	#else
-		tcsetattr (0, TCSANOW, &stored_settings); // 恢复终端参数  
-	#endif  
-	return c;   
+		tcsetattr (0, TCSANOW, &stored_settings); // 恢复终端参数
+	#endif
+	return c;
 }
 
 int addCommand(string name,string cmd){//向命令列表中添加一条命令
@@ -265,7 +265,7 @@ int brainfuck(string cmd){//解析命令
 					}
 					break;
 				case ','://输入
-					storage[pos]=_getch();
+					storage[pos]=Getch();
 					break;
 				case '.'://输出
 					putchar(storage[pos]);
